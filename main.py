ef891cb3c19f78135fcdb7d65d43b16a6ef51c9f
SHWETA <74524265+shwe0601@users.noreply.github.com>
2021-07-08 15:57:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 #This can be your choice
 BUY_PRICE=200000
-#You can choose whicheve product you like
+#You can choose whichever product you like
 URL = "https://www.amazon.in/New-Apple-iPhone-Pro-256GB/dp/B08L5T31M6/ref=sr_1_1_sspa?dchild=1&keywords=iphone+12+pro+max&qid=1625735626&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExR01QMFJIM0g2NFlMJmVuY3J5cHRlZElkPUEwOTY0NDUxMjZDUzdTRzBMTVNEWCZlbmNyeXB0ZWRBZElkPUEwMjY3MTgxM0hET0RaNTlXRDZJRCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome",
@@ -24,7 +24,7 @@ title = soup.find(id="productTitle").get_text().strip()
 if new_price<BUY_PRICE:
     message=f"{title} is now {new_price} BUY SOON"
 
-
+#Make sure to use your own mail_id and password or use environmental variables to hide them.
     with smtplib.SMTP(YOUR_SMTP_ADDRESS) as connection:
         connection.starttls()
         result=connection.login(user=YOUR_EMAIL,password=YOUR_PASSWORD)
